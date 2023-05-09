4755439d8d95def383863384537117f7cd5a992a
Gregory Bowne <gbowne144@gmail.com>
2023-05-09 16:24:43 -0700

    Adding intial commit
    Initial commit

---
+++ game.h
@@ -0,0 +1,32 @@
+#ifndef GAME_H
+#define GAME_H
+
+// Define constants for the playing field dimensions
+const int PLAYING_FIELD_WIDTH = 80;
+const int PLAYING_FIELD_HEIGHT = 24;
+
+// Define the characters used for the playing field
+const char PLAYING_FIELD_VERTICAL_BORDER = '|';
+const char PLAYING_FIELD_HORIZONTAL_BORDER = '-';
+const char PLAYING_FIELD_CORNER = '_';
+const char PLAYING_FIELD_STAR = '*';
+const char PLAYING_FIELD_SHIP = 'S';
+const char PLAYING_FIELD_PLANET = 'P';
+
+// Define the struct for a player
+struct Player {
+    int score;
+};
+
+// Declare functions
+void display_menu();
+void initialize_playing_field();
+void display_playing_field();
+bool game_over();
+void handle_user_input();
+void update_playing_field();
+void handle_collisions();
+void handle_scoring();
+void display_high_score();
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,169 @@
+#include "game.h"
+#include <iostream>
+
+using namespace std;
+
+// Declare global variables
+Player players[2];
+int currentPlayerIndex = 0;
+char playingField[PLAYING_FIELD_HEIGHT][PLAYING_FIELD_WIDTH];
+
+int main()
+{
+	display_menu();
+	initialize_playing_field();
+	display_playing_field();
+
+	while (!game_over())
+	{
+		handle_user_input();
+		update_playing_field();
+		display_playing_field();
+		handle_collisions();
+	}
+
+	handle_scoring();
+	display_high_score();
+	return 0;
+}
+
+void display_menu()
+{
+	// Display the name of the game and ask for the number of players
+	cout << "Welcome to Space Game!" << endl;
+	cout << "How many players? (1 or 2)" << endl;
+
+	// Read the number of players from the keyboard
+	int numPlayers;
+	cin >> numPlayers;
+
+	// Initialize the players' scores to 0
+	for (int i = 0; i < numPlayers; i++)
+	{
+		players[i].score = 0;
+	}
+
+	// Display the high score and prompt the user to start the game
+	cout << "High Score: " << players[0].score << endl;
+	cout << "Press any key to start the game..." << endl;
+	cin.ignore();
+	cin.get();
+}
+
+void initialize_playing_field()
+{
+	// Initialize the playing field with stars
+	for (int i = 0; i < PLAYING_FIELD_HEIGHT; i++)
+	{
+		for (int j = 0; j < PLAYING_FIELD_WIDTH; j++)
+		{
+			playingField[i][j] = PLAYING_FIELD_STAR;
+		}
+	}
+
+	// Add a ship and a planet to the playing field
+	playingField[10][10] = PLAYING_FIELD_SHIP;
+	playingField[15][20] = PLAYING_FIELD_PLANET;
+}
+
+void display_playing_field()
+{
+	// Clear the console window
+	system("clear");
+
+	// Display the playing field
+	for (int i = 0; i < PLAYING_FIELD_HEIGHT; i++)
+	{
+		for (int j = 0; j < PLAYING_FIELD_WIDTH; j++)
+		{
+			cout << playingField[i][j];
+		}
+		cout << endl;
+	}
+}
+
+bool game_over()
+{
+	// Game over if the ship collides with the planet
+	return playingField[10][10] == PLAYING_FIELD_PLANET;
+}
+
+void handle_user_input()
+{
+	// Move the ship based on user input
+	char input;
+	cout << "Player " << currentPlayerIndex + 1 << ", enter your move (WASD): ";
+	cin >> input;
+
+	switch (input)
+	{
+	case 'w':
+		playingField[10][10] = PLAYING_FIELD_STAR;
+		playingField[9][10] = PLAYING_FIELD_SHIP;
+		break;
+	case 'a':
+		playingField[10][10] = PLAYING_FIELD_STAR;
+		playingField[10][9] = PLAYING_FIELD_SHIP;
+		break;
+	case 's':
+		playingField[10][10] = PLAYING_FIELD_STAR;
+		playingField[11][10] = PLAYING_FIELD_SHIP;
+		break;
+	case 'd':
+		playingField[10][10] = PLAYING_FIELD_STAR;
+		playingField[10][11] = PLAYING_FIELD_SHIP;
+		break;
+	default:
+		break;
+	}
+}
+
+void update_playing_field()
+{
+	// Update the playing field based on game logic
+}
+
+void handle_collisions()
+{
+	// Handle collisions between objects in the game
+	if (playingField[10][10] == PLAYING_FIELD_PLANET)
+	{
+		players[currentPlayerIndex].score -= 10;
+	}
+}
+
+void handle_scoring()
+{
+	// Update the player's score based on the game's outcome
+	if (playingField[10][10] == PLAYING_FIELD_PLANET)
+	{
+		players[currentPlayerIndex].score -= 10;
+	}
+	else
+	{
+		players[currentPlayerIndex].score += 5;
+	}
+
+	// Switch to the next player
+	currentPlayerIndex = (currentPlayerIndex + 1) % 2;
+}
+
+void display_high_score()
+{
+	// Check which player has the highest score
+	int highestScore = players[0].score;
+	int highestScoreIndex = 0;
+
+	for (int i = 1; i < 2; i++)
+	{
+		if (players[i].score > highestScore)
+		{
+			highestScore = players[i].score;
+			highestScoreIndex = i;
+		}
+	}
+
+	// Display the high score and the name of the winning player
+	cout << "High Score: " << highestScore << endl;
+	cout << "Player " << highestScoreIndex + 1 << " wins!" << endl;
+}
