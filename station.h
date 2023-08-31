3c00d9b74c24a5e1e3162083445825d81f82f70b
Greg Bowne <gbowne144@gmail.com>
2023-08-31 15:44:47 -0700

    Removing the ./game executable artifact and fixed station.h but still may be issues with station.cpp etc

--- station.h
+++ station.h
@@ -1,39 +1,27 @@
-#include "station.h"
+#ifndef STATION_H
+#define STATION_H
 
-Station::Station(std::string name, int maxHealth, int maxFuel, int maxAmmo)
-    : name_{name},
-      health_{maxHealth},
-      fuel_{maxFuel},
-      ammo_{maxAmmo},
-      maxHealth_{maxHealth},
-      maxFuel_{maxFuel},
-      maxAmmo_{maxAmmo} {}
+#include <string>
 
-std::string Station::getName() const { return name_; }
+class Station {
+public:
+    Station(std::string name, int maxHealth, int maxFuel, int maxAmmo);
+    std::string getName() const;
+    int getHealth() const;
+    int getFuel() const;
+    int getAmmo() const;
+    void repair(int amount);
+    void refuel(int amount);
+    void restock(int amount);
 
-int Station::getHealth() const { return health_; }
+private:
+    std::string name_;
+    int health_;
+    int fuel_;
+    int ammo_;
+    int maxHealth_;
+    int maxFuel_;
+    int maxAmmo_;
+};
 
-int Station::getFuel() const { return fuel_; }
-
-int Station::getAmmo() const { return ammo_; }
-
-void Station::repair(int amount) {
-    health_ += amount;
-    if (health_ > maxHealth_) {
-        health_ = maxHealth_;
-    }
-}
-
-void Station::refuel(int amount) {
-    fuel_ += amount;
-    if (fuel_ > maxFuel_) {
-        fuel_ = maxFuel_;
-    }
-}
-
-void Station::restock(int amount) {
-    ammo_ += amount;
-    if (ammo_ > maxAmmo_) {
-        ammo_ = maxAmmo_;
-    }
-}
+#endif // STATION_H
