71dbd7cd334ff8ac92e054f2e5fccd41d3cf9fcb
Gregory Bowne <gbowne144@gmail.com>
2025-04-01 21:40:07 -0700

    Update Menu.h
    updated Menu.h

--- include/ui/Menu.h
+++ include/ui/Menu.h
@@ -3,7 +3,6 @@
 
 #include <limits>
 #include <string>
-
 #include "../../include/game_logic/Game.h"
 #include "../../include/ui/Display.h"
 
@@ -22,19 +21,12 @@ class Menu {
     ~Menu();
 
     void displayMenu();
-    Menu::Action handleInput();
+    Action handleInput();
     void handleMenuAction(Action action);
 
   private:
     Display& display;
     Game& game;
-
-    // Remove these private methods as they were causing conflicts
-    // void startGame();
-    // void selectPlayers();
-    // void loadGame();
-    // void saveGame();
-    // void exitGame();
 };
 
 #endif  // MENU_H
